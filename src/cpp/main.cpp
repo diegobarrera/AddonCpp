b09756fca55a7f4b6db242dab892be184c876a03
Diego Sebastian Barrera Guevara <diego.barrera@globant.com>
2019-05-02 16:07:26 -0500

    add library cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,7 +1,22 @@
-#include <napi.h>
+#include "abstract.h"
 
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return exports;
+std::string functionexample::hello(){
+  return "Hello World";
 }
 
-NODE_API_MODULE(testaddon, InitAll)
+Napi::String functionexample::HelloWrapped(const Napi::CallbackInfo& info) 
+{
+  Napi::Env env = info.Env();
+  Napi::String returnValue = Napi::String::New(env, functionexample::hello());
+  
+  return returnValue;
+}
+
+Napi::Object functionexample::Init(Napi::Env env, Napi::Object exports) 
+{
+  exports.Set(
+"hello", Napi::Function::New(env, functionexample::HelloWrapped)
+  );
+ 
+  return exports;
+}
